661fec60e1d5772e9d1458f5b9fa798a539e8b80
Priyansh Thakar <priyanshthakar@gmail.com>
2023-07-24 11:55:00 -0400

    Added getting variables from view.

--- app/src/main/java/com/georgiancollege/week12/MainActivity.java
+++ app/src/main/java/com/georgiancollege/week12/MainActivity.java
@@ -3,12 +3,47 @@ package com.georgiancollege.week12;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText number1TextField;
+    private EditText number2TextField;
+    private Button submitButton;
+    private TextView finalLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Initialize", "We are inside our First View.");
+
+        // getting the ids from the view
+        number1TextField = findViewById(R.id.editTextNumber);
+        number2TextField = findViewById(R.id.editTextNumber2);
+        submitButton = findViewById(R.id.button);
+        finalLabel = findViewById(R.id.textView2);
+
+        // submit button click listener
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Listener", "Submit Button is clicked!");
+
+                try {
+                    int num1 = Integer.parseInt(number1TextField.getText().toString());
+                    int num2 = Integer.parseInt(number2TextField.getText().toString());
+                    Log.d("NumberValues", "Number 1: " + num1);
+                    Log.d("NumberValues", "Number 2: " + num2);
+                } catch (Exception e) {
+                    finalLabel.setText("Please enter both the numbers!");
+                }
+            }
+        });
     }
 }
